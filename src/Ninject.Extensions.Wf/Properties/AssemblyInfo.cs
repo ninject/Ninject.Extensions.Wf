b6c63cae9ac9abceebd635ad15b08c5b1f4a0c32
Remo Gloor <remo.gloor@bbv.ch>
2011-12-27 21:16:53 +0100

    Added assembly description for nuget

--- src/Ninject.Extensions.Wf/Properties/AssemblyInfo.cs
+++ src/Ninject.Extensions.Wf/Properties/AssemblyInfo.cs
@@ -24,6 +24,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ninject.Extensions.Wf")]
+[assembly: AssemblyDescriptionAttribute("Ninject extension to add IoC support to Windows Workflow Foundation 4")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2b81f166-77d4-4f61-b736-24878c693aaa")]
